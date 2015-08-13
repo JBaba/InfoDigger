99c4725c20c65dc3bc05029b7e9199ff52185074
jbaba <naimish.viradia@yahoo.com>
2015-08-13 10:51:48 -0400

    added scanner approch to find who is fastest from bufferreader, jsoup

--- src/com/info/digger/testing/code/Crawler.java
+++ src/com/info/digger/testing/code/Crawler.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,38 +15,71 @@ public class Crawler {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 
-		Crawler c=new Crawler();
-		System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+		Crawler c = new Crawler();
+		System.setProperty("http.agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
 		String a = null;
 		a = "http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array";
 
 		c.getHTML(a);
 		c.getHTMLJsoup(a);
 		c.getHTML(a);
-		
+
+		c.getScanner(a);
+		c.getHTMLJsoup(a);
 	}
-	
+
+	public void getScanner(String a) {
+
+		long tStart = System.currentTimeMillis();
+
+		String content = null;
+		URLConnection connection = null;
+		try {
+			connection = new URL(a).openConnection();
+			Scanner scanner = new Scanner(connection.getInputStream());
+			scanner.useDelimiter("\\Z");
+			StringBuffer html = new StringBuffer();
+			while (scanner.hasNext()) {
+				html.append(scanner.next());
+
+			}
+
+			Document doc = Jsoup.parse(html.toString()); // parsing
+
+			long tEnd = System.currentTimeMillis();
+			long tDelta = tEnd - tStart;
+			double elapsedSeconds = tDelta / 1000.0;
+			System.out.println("Time :" + elapsedSeconds);
+
+			// System.out.println(html.toString());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
 	public Document getHTMLJsoup(String URL) throws IOException {
 		Document doc = null;
-		
+
 		long tStart = System.currentTimeMillis();
-		
+
 		doc = Jsoup.connect(URL).get();
-		//System.out.println(doc.text());
-		
+		// System.out.println(doc.text());
+
 		long tEnd = System.currentTimeMillis();
 		long tDelta = tEnd - tStart;
 		double elapsedSeconds = tDelta / 1000.0;
 		System.out.println("Time :" + elapsedSeconds);
-		
+
 		return doc;
 	}
-	
+
 	public Document getHTML(String URL) {
 		long tStart = System.currentTimeMillis();
-		
+
 		URL url;
-		
+
 		try {
 			// get URL content
 			url = new URL(URL);
@@ -60,14 +94,14 @@ public class Crawler {
 				html.append(inputLine);
 			}
 			br.close();
-			
-			Document doc = Jsoup.parse(html.toString()); // parsing 
-			
+
+			Document doc = Jsoup.parse(html.toString()); // parsing
+
 			long tEnd = System.currentTimeMillis();
 			long tDelta = tEnd - tStart;
 			double elapsedSeconds = tDelta / 1000.0;
 			System.out.println("Time :" + elapsedSeconds);
-			
+
 			return doc;
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -76,5 +110,5 @@ public class Crawler {
 		}
 		return null;
 	}
-	
+
 }
