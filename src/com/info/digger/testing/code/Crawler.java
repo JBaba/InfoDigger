d16fa817bc21a5652b44e19280a54b61f9e59bb7
jbaba <naimish.viradia@yahoo.com>
2015-08-13 10:45:35 -0400

    Compare parsing is faster after getting html. or use jsoup to get doc

--- src/com/info/digger/testing/code/Crawler.java
+++ src/com/info/digger/testing/code/Crawler.java
@@ -1,6 +1,11 @@
 package com.info.digger.testing.code;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,19 +14,67 @@ public class Crawler {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 
-		long tStart = System.currentTimeMillis();
-
+		Crawler c=new Crawler();
+		System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
 		String a = null;
-		a = "http://www.cybercoders.com/search/?page=1&searchterms=java&searchlocation=&newsearch=true&sorttype=date";
+		a = "http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array";
 
-		Document doc = Jsoup.connect(a).get();
-		System.out.println(doc.text());
+		c.getHTML(a);
+		c.getHTMLJsoup(a);
+		c.getHTML(a);
+		
+	}
+	
+	public Document getHTMLJsoup(String URL) throws IOException {
+		Document doc = null;
+		
+		long tStart = System.currentTimeMillis();
+		
+		doc = Jsoup.connect(URL).get();
+		//System.out.println(doc.text());
 		
 		long tEnd = System.currentTimeMillis();
 		long tDelta = tEnd - tStart;
 		double elapsedSeconds = tDelta / 1000.0;
 		System.out.println("Time :" + elapsedSeconds);
+		
+		return doc;
+	}
+	
+	public Document getHTML(String URL) {
+		long tStart = System.currentTimeMillis();
+		
+		URL url;
+		
+		try {
+			// get URL content
+			url = new URL(URL);
+			URLConnection conn = url.openConnection();
+			StringBuffer html = new StringBuffer();
 
+			// open the stream and put it into BufferedReader
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String inputLine;
+			while ((inputLine = br.readLine()) != null) {
+				html.append(inputLine);
+			}
+			br.close();
+			
+			Document doc = Jsoup.parse(html.toString()); // parsing 
+			
+			long tEnd = System.currentTimeMillis();
+			long tDelta = tEnd - tStart;
+			double elapsedSeconds = tDelta / 1000.0;
+			System.out.println("Time :" + elapsedSeconds);
+			
+			return doc;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 }
