493a7d904b0a6e328a44d72231a672965283668a
jbaba <naimish.viradia@yahoo.com>
2015-08-13 10:52:48 -0400

    added http.agent to read all websites

--- src/com/info/digger/web/InvokeUrl.java
+++ src/com/info/digger/web/InvokeUrl.java
@@ -33,6 +33,10 @@ public class InvokeUrl {
 	private List<JobModal> jobs= null;
 
 	public InvokeUrl() {
+		
+		System.setProperty("http.agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+		
 		jobIds = new LinkedList<String>();
 		jobs = new LinkedList<JobModal>();
 	}
